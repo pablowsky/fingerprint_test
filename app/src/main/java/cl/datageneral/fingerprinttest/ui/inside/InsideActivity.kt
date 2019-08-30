d8874355a80b7d3ea1636a68f0cf8de477562b6b
Pablo Molina <edsacm@gmail.com>
2019-08-30 17:08:53 -0400

    activity Inside created

---
+++ app/src/main/java/cl/datageneral/fingerprinttest/ui/inside/InsideActivity.kt
@@ -0,0 +1,13 @@
+package cl.datageneral.fingerprinttest.ui.inside
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cl.datageneral.fingerprinttest.R
+
+class InsideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inside)
+    }
+}
